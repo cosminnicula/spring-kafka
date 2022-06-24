2211ad29cd5549f98fcbab123d1d32954d9ce1a5
Cosmin Nicula <582616+cosminnicula@users.noreply.github.com>
2022-06-24 23:26:44 +0300

    Kafka producer callback

--- kafka-ms-order/src/main/java/dev/intermediatebox/broker/producer/OrderProducer.java
+++ kafka-ms-order/src/main/java/dev/intermediatebox/broker/producer/OrderProducer.java
@@ -1,16 +1,35 @@
 package dev.intermediatebox.broker.producer;
 
 import dev.intermediatebox.broker.message.OrderMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 public class OrderProducer {
+  private static final Logger log = LoggerFactory.getLogger(OrderProducer.class);
+
   @Autowired
   private KafkaTemplate<String, OrderMessage> kafkaTemplate;
 
   public void publish(OrderMessage orderMessage) {
-    kafkaTemplate.send("t-commodity-order", orderMessage.getOrderNumber(), orderMessage);
+    kafkaTemplate.send("t-commodity-order", orderMessage.getOrderNumber(), orderMessage)
+        .addCallback(new ListenableFutureCallback<SendResult<String, OrderMessage>>() {
+          @Override
+          public void onSuccess(SendResult<String, OrderMessage> result) {
+            log.info("Order {}, item {} published successfully", orderMessage.getOrderNumber(), orderMessage.getItemName());
+          }
+
+          @Override
+          public void onFailure(Throwable ex) {
+            log.warn("Order {}, item {} failed to publish {}", orderMessage.getOrderNumber(), orderMessage.getItemName(),
+                ex.getMessage());
+          }
+        });
+    log.info("Dummy log for order {}, item {}", orderMessage.getOrderNumber(), orderMessage.getItemName());
   }
 }
